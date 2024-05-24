ec0c57c1081a3785cd86d332a369862719cb7105
chengmoxu <rolly200311@gmail.com>
2024-05-24 13:47:32 +0800

    Cookies Support Function Add

---
+++ wahid-dl-wincmd Cookies Support.bat
@@ -0,0 +1,14 @@
+@echo off
+echo wahid-dl Cookies Support
+echo ------------------------------------------------------------
+set /p var=請輸入欲下載影片之網址:
+echo ------------------------------------------------------------
+echo 下載開始
+echo ------------------------------------------------------------
+cd C:\wahid-dl
+yt-dlp -U
+yt-dlp --cookies-from-browser chrome -c -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 %var%
+echo ------------------------------------------------------------
+echo 執行結束，請至資料夾內確認您的下載
+echo ------------------------------------------------------------
+pause
