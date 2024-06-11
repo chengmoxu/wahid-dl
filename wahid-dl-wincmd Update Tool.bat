9cacff509fae56acf85f11cbb4df03e44abd1937
chengmoxu <rolly200311@gmail.com>
2024-06-11 22:00:16 +0800

    Bugs fix

--- wahid-dl-wincmd Update Tool.bat
+++ wahid-dl-wincmd Update Tool.bat
@@ -4,26 +4,26 @@ echo ------------------------------------------------------------
 echo 開始執行
 echo ------------------------------------------------------------
 echo 更新 yt-dlp
-cd C:\wahid-dl
+cd "C:\wahid-dl"
 yt-dlp -U
 echo ------------------------------------------------------------
 echo 更新 ffmpeg
-cd C:\wahid-dl
+cd "C:\wahid-dl"
 curl -o ffmpeg.zip https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip
 mkdir ffmpeg
-tar -zxvf ffmpeg.zip -C C:\wahid-dl\ffmpeg
-move C:\wahid-dl\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\*.exe C:\wahid-dl\
+tar -zxvf ffmpeg.zip -C "C:\wahid-dl\ffmpeg"
+move "C:\wahid-dl\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\*.exe" "C:\wahid-dl\" -Force
 del ffmpeg.zip
-rd /s/q C:\wahid-dl\ffmpeg
+rd /s/q "C:\wahid-dl\ffmpeg"
 echo ------------------------------------------------------------
 echo 更新 wahid-dl
-cd C:\wahid-dl
+cd "C:\wahid-dl"
 curl -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main
 mkdir updates
-tar -zxvf updates.zip -C C:\wahid-dl\updates
-move C:\wahid-dl\updates\wahid-dl-main\*.bat C:\wahid-dl\
+tar -zxvf updates.zip -C "C:\wahid-dl\updates"
+move "C:\wahid-dl\updates\wahid-dl-main\*.bat" "C:\wahid-dl\" -Force
 del updates.zip
-rd /s/q C:\wahid-dl\updates
+rd /s/q "C:\wahid-dl\updates"
 echo ------------------------------------------------------------
 echo 執行完成
 echo ------------------------------------------------------------
