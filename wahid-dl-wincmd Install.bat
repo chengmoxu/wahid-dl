3fbd9464e41630a9f2ffccb1b9e770229d34dfb6
chengmoxu <rolly200311@gmail.com>
2024-06-14 08:20:40 +0800

    Code optimization and bugs fix

--- wahid-dl-wincmd Install.bat
+++ wahid-dl-wincmd Install.bat
@@ -7,6 +7,15 @@ echo 建立資料夾
 cd "C:\"
 mkdir wahid-dl
 echo ------------------------------------------------------------
+echo 安裝 wahid-dl
+cd "C:\wahid-dl"
+curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main
+mkdir updates
+tar -zxvf updates.zip -C "C:\wahid-dl\updates"
+move "C:\wahid-dl\updates\wahid-dl-main\*.bat" "C:\wahid-dl\"
+del "C:\wahid-dl\updates.zip"
+rd /s /q "C:\wahid-dl\updates"
+echo ------------------------------------------------------------
 echo 下載 yt-dlp
 cd "C:\wahid-dl"
 curl -L -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
@@ -16,18 +25,9 @@ cd "C:\wahid-dl"
 curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/7.0.1/ffmpeg-7.0.1-full_build.zip
 mkdir "C:\wahid-dl\ffmpeg"
 tar -zxvf ffmpeg.zip -C "C:\wahid-dl\ffmpeg"
-move "C:\wahid-dl\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\*.exe" "C:\wahid-dl\"
-del ffmpeg.zip
+move "C:\wahid-dl\ffmpeg\ffmpeg-7.0.1-full_build\bin\*.exe" "C:\wahid-dl\"
+del "C:\wahid-dl\ffmpeg.zip"
 rd /s /q "C:\wahid-dl\ffmpeg"
-echo ------------------------------------------------------------
-echo 安裝 wahid-dl
-cd "C:\wahid-dl"
-curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main
-mkdir updates
-tar -zxvf updates.zip -C "C:\wahid-dl\updates"
-move "C:\wahid-dl\updates\wahid-dl-main\*.bat" "C:\wahid-dl\"
-del updates.zip
-rd /s /q "C:\wahid-dl\updates"
 echo ------------------------------------------------------------
 echo 執行完成
 echo ------------------------------------------------------------
--- wahid-dl-wincmd Update Tool.bat
+++ wahid-dl-wincmd Update Tool.bat
@@ -7,23 +7,23 @@ echo 更新 yt-dlp
 cd "C:\wahid-dl"
 yt-dlp -U
 echo ------------------------------------------------------------
-echo 更新 ffmpeg
-cd "C:\wahid-dl"
-curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/7.0.1/ffmpeg-7.0.1-full_build.zip
-mkdir "C:\wahid-dl\ffmpeg"
-tar -zxvf ffmpeg.zip -C "C:\wahid-dl\ffmpeg"
-move "C:\wahid-dl\ffmpeg\ffmpeg-master-latest-win64-gpl\bin\*.exe" "C:\wahid-dl\"
-del ffmpeg.zip
-rd /s /q "C:\wahid-dl\ffmpeg"
-echo ------------------------------------------------------------
 echo 更新 wahid-dl
 cd "C:\wahid-dl"
 curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main
 mkdir "C:\wahid-dl\updates"
 tar -zxvf updates.zip -C "C:\wahid-dl\updates"
 move "C:\wahid-dl\updates\wahid-dl-main\*.bat" "C:\wahid-dl\"
-del updates.zip
+del "C:\wahid-dl\updates.zip"
 rd /s /q "C:\wahid-dl\updates"
+echo ------------------------------------------------------------
+echo 更新 ffmpeg
+cd "C:\wahid-dl"
+curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/7.0.1/ffmpeg-7.0.1-full_build.zip
+mkdir "C:\wahid-dl\ffmpeg"
+tar -zxvf ffmpeg.zip -C "C:\wahid-dl\ffmpeg"
+move "C:\wahid-dl\ffmpeg\ffmpeg-7.0.1-full_build\bin\*.exe" "C:\wahid-dl\"
+del "C:\wahid-dl\ffmpeg.zip"
+rd /s /q "C:\wahid-dl\ffmpeg"
 echo ------------------------------------------------------------
 echo 執行完成
 echo ------------------------------------------------------------
