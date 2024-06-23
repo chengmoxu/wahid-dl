7a9651b8268b24a7d1a5a0d5cd11525cf283b11b
chengmoxu <rolly200311@gmail.com>
2024-06-23 19:09:51 +0800

    Update

--- wahid-dl-wincmd Dev Mode.bat
+++ wahid-dl-wincmd Dev Mode.bat
@@ -1,7 +1,7 @@
 @echo off
 echo wahid-dl Dev Mode
-echo Version: 3.2
-echo Build: wahid-dl.v3.2.20240616.windows-cmd.1
+echo Version: 3.3
+echo Build: wahid-dl.v3.3.20240623.windows-cmd.1
 echo ------------------------------------------------------------
 set /p var=yt-dlp命令模式，請直接輸入yt-dlp命令:
 echo ------------------------------------------------------------
