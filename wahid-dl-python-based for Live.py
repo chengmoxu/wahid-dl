ef810cbf6059a2d3d6c46bcfe7fcd4bbb6fc42dc
chengmoxu <rolly200311@gmail.com>
2024-06-26 16:09:30 +0800

    Add live download function to wahid-dl based on Python

---
+++ wahid-dl-python-based for Live.py
@@ -0,0 +1,41 @@
+# wahid-dl Python-Based for Live
+# Version: 3.4
+# Build: wahid-dl.v3.4.20240625.Python.1
+
+# Library import
+import os
+import sys
+import subprocess
+
+# Download Function within OS judge
+if sys.platform == "win32":
+    print ("wahid-dl Python-Based for Live")
+    print ("------------------------------------------------------------")
+    downloadlink = str (input ('請輸入欲下載影片之網址:'))
+    print ("------------------------------------------------------------")
+    print ("執行開始")
+    print ("------------------------------------------------------------")
+    os.chdir ('C:\\wahid-dl')
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
+    downloadcommand = str (('yt-dlp -c --live-from-start -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 --ffmpeg-location "C:\\FFmpeg" ') + downloadlink)
+    os.system (downloadcommand)
+    print ("------------------------------------------------------------")
+    print ("執行結束，請至資料夾內確認您的下載")
+    print ("------------------------------------------------------------")
+    os.system ('pause')
+elif sys.platform == "linux":
+    print ("wahid-dl Python-Based")
+    print ("------------------------------------------------------------")
+    downloadlink = str (input ('請輸入欲下載影片之網址:'))
+    print ("------------------------------------------------------------")
+    print ("執行開始")
+    print ("------------------------------------------------------------")
+    os.system ('pip install -U "yt-dlp[default]"')
+    downloadcommand = str (('yt-dlp -c --live-from-start -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 ') + downloadlink)
+    os.system (downloadcommand)
+    print ("------------------------------------------------------------")
+    print ("執行結束，請至資料夾內確認您的下載")
+    print ("------------------------------------------------------------")
+
+#elif sys.platform == "darwin":
+#    print ("wahid-dl Python-Based")
