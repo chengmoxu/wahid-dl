ed4ffb769bf9151f69bd3d8bb8825099d1f03eca
chengmoxu <rolly200311@gmail.com>
2024-06-28 15:22:14 +0800

    Python-Based Format Checking Tool add loop and input judgment

--- wahid-dl-python-based Format Checking Tool.py
+++ wahid-dl-python-based Format Checking Tool.py
@@ -11,30 +11,68 @@ import subprocess
 if sys.platform == "win32":
     print ("wahid-dl-python-based Format Checking Tool")
     print ("------------------------------------------------------------")
-    testlink = str (input ('請輸入欲測試影片或音檔之網址:'))
-    print ("------------------------------------------------------------")
-    print ("執行開始")
-    print ("------------------------------------------------------------")
-    os.chdir ('C:\\wahid-dl')
-    subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
-    testcommand = str (('yt-dlp -F ') + testlink)
-    os.system (testcommand)
-    print ("------------------------------------------------------------")
-    print ("執行結束，測試結果已於上方顯示")
-    print ("------------------------------------------------------------")
+    mode = ""
+    while mode != "0":
+        print ("請輸入欲測試影片或音檔之網址")
+        print ("或者，想要結束程式請輸入0\n")
+        userinput = input ("請輸入：")
+        userinput_judge = str.isdigit(userinput)
+        if userinput_judge == True: 
+            if  userinput == "0":
+                mode = "0"
+            else:
+               print ("請重新輸入正確選項！")
+        elif userinput_judge == False:
+            if userinput.startswith("http") == True:
+                print ("------------------------------------------------------------")
+                print ("執行開始")
+                print ("------------------------------------------------------------")
+                os.chdir ('C:\\wahid-dl')
+                subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
+                testcommand = str (('yt-dlp -F ') + userinput)
+                os.system (testcommand)
+                print ("------------------------------------------------------------")
+                print ("執行結束")
+                print ("------------------------------------------------------------")
+            elif userinput.startswith("http") == False:
+                print ("請重新輸入正確網址！")
+            else:
+                print ("請重新輸入正確網址！")
+    while mode == "0":
+        print("即將結束程式")
+        break
 elif sys.platform == "linux":
     print ("wahid-dl-python-based Format Checking Tool")
     print ("------------------------------------------------------------")
-    testlink = str (input ('請輸入欲測試影片或音檔之網址:'))
-    print ("------------------------------------------------------------")
-    print ("執行開始")
-    print ("------------------------------------------------------------")
-    subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
-    testcommand = str (('yt-dlp -F ') + testlink)
-    os.system (testcommand)
-    print ("------------------------------------------------------------")
-    print ("執行結束，測試結果已於上方顯示")
-    print ("------------------------------------------------------------")
+    mode = ""
+    while mode != "0":
+        print ("請輸入欲測試影片或音檔之網址")
+        print ("或者，想要結束程式請輸入0\n")
+        userinput = input ("請輸入：")
+        userinput_judge = str.isdigit(userinput)
+        if userinput_judge == True: 
+            if  userinput == "0":
+                mode = "0"
+            else:
+               print ("請重新輸入正確選項！")
+        elif userinput_judge == False:
+            if userinput.startswith("http") == True:
+                print ("------------------------------------------------------------")
+                print ("執行開始")
+                print ("------------------------------------------------------------")
+                subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
+                testcommand = str (('yt-dlp -F ') + userinput)
+                os.system (testcommand)
+                print ("------------------------------------------------------------")
+                print ("執行結束")
+                print ("------------------------------------------------------------")
+            elif userinput.startswith("http") == False:
+                print ("請重新輸入正確網址！")
+            else:
+                print ("請重新輸入正確網址！")
+    while mode == "0":
+        print("即將結束程式")
+        break
 #elif sys.platform == "darwin":
 #    print ("wahid-dl Python-Based")
 os.system ('pause')
