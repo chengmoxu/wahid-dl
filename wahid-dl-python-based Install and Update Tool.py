4b55ebb29fdd3fecd02b59dbd792f46f2d40c944
Hong-Yi, Xu <rolly200311@gmail.com>
2024-08-11 22:31:16 +0800

    style: Removed old annotation

--- wahid-dl-python-based Install and Update Tool.py
+++ wahid-dl-python-based Install and Update Tool.py
@@ -46,7 +46,6 @@ if sys.platform == "win32":
                 print ("正在確認 wahid-dl 資料夾存在與否")
                 os.chdir ('C:\\')
                 # 判斷wahid-dl資料夾存在與否
-                #os.mkdir ('wahid-dl')
                 wahiddl_folder_name = 'wahid-dl'
                 if not os.path.exists (wahiddl_folder_name):
                     os.mkdir (wahiddl_folder_name)
@@ -55,7 +54,6 @@ if sys.platform == "win32":
                     print (f"{wahiddl_folder_name} 資料夾已存在，本工具將進行更新 wahid-dl 以及附屬依賴工具")
                     os.chdir ('C:\\wahid-dl')
                     # 移除舊版*.bat
-                    #os.system ('del *.bat')
                     wahiddl_old_bat_files = glob.glob ("*.bat")
                     for bat_file in wahiddl_old_bat_files:
                         if os.path.exists (bat_file):
@@ -64,7 +62,6 @@ if sys.platform == "win32":
                         else:
                             print ("不存在 wahid-dl 舊版之 .bat 檔案")
                     # 移除舊版*.py
-                    #os.system ('del *.py')
                     os.chdir ('C:\\wahid-dl')
                     wahiddl_old_py_files = glob.glob ("*.py")
                     for py_file in wahiddl_old_py_files:
@@ -78,7 +75,6 @@ if sys.platform == "win32":
                 print ("安裝/更新 wahid-dl")
                 os.chdir ('C:\\wahid-dl')
                 os.system ('curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main')
-                #os.mkdir ('updates')
                 updates_folder_name = 'updates'
                 if not os.path.exists (updates_folder_name):
                     os.mkdir (updates_folder_name)
@@ -87,8 +83,6 @@ if sys.platform == "win32":
                     print (f"{updates_folder_name} 更新資料之暫存資料夾已存在")
                 print ('解壓縮 wahid-dl 更新資料')
                 os.system ('tar -zxvf updates.zip -C "C:\\wahid-dl\\updates"')
-                #os.system ('move "C:\\wahid-dl\\updates\\wahid-dl-main\\*.bat" "C:\\wahid-dl\\"')
-                #os.system ('move "C:\\wahid-dl\\updates\\wahid-dl-main\\*.py" "C:\\wahid-dl\\"')
                 wahiddl_updatesfiles_folder = "C:\\wahid-dl\\updates\\wahid-dl-main\\"
                 wahiddl_dst_folder = "C:\\wahid-dl\\"
                 wahiddl_bat_files = glob.glob (wahiddl_updatesfiles_folder + "*.bat")
@@ -97,14 +91,12 @@ if sys.platform == "win32":
                 wahiddl_py_files = glob.glob (wahiddl_updatesfiles_folder + "*.py")
                 for wahiddl_py_file in wahiddl_py_files:
                     shutil.move (wahiddl_py_file, wahiddl_dst_folder)
-                #os.system ('del "C:\\wahid-dl\\updates.zip"')
                 wahiddl_updates_file_path = "C:\\wahid-dl\\updates.zip"
                 if os.path.isfile (wahiddl_updates_file_path) == True:
                     os.remove (wahiddl_updates_file_path)
                     print (f"更新資料 '{wahiddl_updates_file_path}' 已刪除")
                 else:
                     print (f"更新資料 '{wahiddl_updates_file_path}' 已不存在")
-                #os.system ('rd /s /q "C:\\wahid-dl\\updates"')
                 wahiddl_updates_folder_path = "C:\\wahid-dl\\updates"
                 if os.path.exists (wahiddl_updates_folder_path):
                     shutil.rmtree (wahiddl_updates_folder_path)
@@ -125,7 +117,6 @@ if sys.platform == "win32":
                 print ("下載/更新 yt-dlp 完成")
                 print ("------------------------------------------------------------")
                 print ("下載/更新 yt-dlp Pip Version")
-                #os.system ('pip install -U "yt-dlp[default]"')
                 subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
                 print ("下載/更新 yt-dlp Pip Version 完成")
                 print ("------------------------------------------------------------")
@@ -133,7 +124,6 @@ if sys.platform == "win32":
                 os.chdir ('C:\\wahid-dl')
                 # 移除舊有ffmpeg安裝方式之檔案
                 # 移除舊式FFmpeg安裝之舊版
-                #os.system ('del ffmpeg.exe ffplay.exe ffprobe.exe')
                 if os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe') == True:
                     os.remove ('C:\\wahid-dl\\ffmpeg.exe')
                     print ("已刪除舊式舊版的 ffmpeg.exe")
@@ -153,14 +143,12 @@ if sys.platform == "win32":
                 os.chdir ('C:\\')
                 # 移除新式FFmpeg安裝之舊版
                 ffmpeg_old_ver_folder_path = "C:\\FFmpeg"
-                #os.mkdir ('FFmpeg')
                 ffmpeg_folder_name = 'FFmpeg'
                 if not os.path.exists (ffmpeg_folder_name):
                     os.mkdir (ffmpeg_folder_name)
                     print (f"新式 {ffmpeg_folder_name} 資料夾已建立")
                 else:
                     print (f"新式 {ffmpeg_folder_name} 資料夾已存在")
-                    #os.system ('del ffmpeg.exe ffplay.exe ffprobe.exe')
                     # 移除新式舊版ffmpeg.exe, ffplay.exe, ffprobe.exe
                     if os.path.isfile ('C:\\FFmpeg\\ffmpeg.exe') == True:
                         os.remove ('C:\\FFmpeg\\ffmpeg.exe')
@@ -180,7 +168,6 @@ if sys.platform == "win32":
                 # 安裝/更新新版FFmpeg主程式
                 os.chdir ('C:\\FFmpeg')
                 os.system ('curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/7.0.2/ffmpeg-7.0.2-full_build.zip')
-                #os.mkdir ('FFmpeg-unzip')
                 FFmpegunzip_folder_name = 'FFmpeg-unzip'
                 if not os.path.exists (FFmpegunzip_folder_name):
                     os.mkdir (FFmpegunzip_folder_name)
@@ -188,7 +175,6 @@ if sys.platform == "win32":
                 else:
                     print (f"'{FFmpegunzip_folder_name}' 資料夾已存在")
                 os.system ('tar -zxvf ffmpeg.zip -C "C:\\FFmpeg\\FFmpeg-unzip"')
-                #os.system ('move "C:\\FFmpeg\\FFmpeg-unzip\\ffmpeg-7.0.1-full_build\\bin\\*.exe" "C:\\FFmpeg\\"')
                 ffmpeg_updatesfiles_folder = "C:\\FFmpeg\\FFmpeg-unzip\\ffmpeg-7.0.1-full_build\\bin\\"
                 ffmpeg_dst_folder = "C:\\FFmpeg\\"
                 ffmpeg_exe_files = glob.glob (ffmpeg_updatesfiles_folder + "*.exe")
@@ -196,14 +182,12 @@ if sys.platform == "win32":
                     shutil.move (ffmpeg_exe_file, ffmpeg_dst_folder)
                 # FFmpeg系統環境變數設定
                 os.system ('setx PATH "FFmpeg;C:\\FFmpeg\\"')
-                #os.system ('del "C:\\FFmpeg\\ffmpeg.zip"')
                 ffmpeg_updates_file_path = "C:\\FFmpeg\\ffmpeg.zip"
                 if os.path.isfile (ffmpeg_updates_file_path) == True:
                     os.remove (ffmpeg_updates_file_path)
                     print (f"更新資料 '{ffmpeg_updates_file_path}' 已刪除")
                 else:
                     print (f"更新資料 '{ffmpeg_updates_file_path}' 已不存在")
-                #os.system ('rd /s /q "C:\\FFmpeg\\ffmpeg-unzip"')
                 ffmpeg_unzip_folder_path = "C:\\FFmpeg\\ffmpeg-unzip"
                 if os.path.exists (ffmpeg_unzip_folder_path):
                     shutil.rmtree (ffmpeg_unzip_folder_path)
@@ -219,7 +203,6 @@ if sys.platform == "win32":
                 print ("正在確認 wahid-dl 資料夾存在與否")
                 os.chdir ('C:\\')
                 # 判斷wahid-dl資料夾存在與否
-                #os.mkdir ('wahid-dl')
                 wahiddl_folder_name = 'wahid-dl'
                 if not os.path.exists (wahiddl_folder_name):
                     os.mkdir (wahiddl_folder_name)
@@ -228,7 +211,6 @@ if sys.platform == "win32":
                     print (f"{wahiddl_folder_name} 資料夾已存在，本工具將進行更新 wahid-dl")
                     os.chdir ('C:\\wahid-dl')
                     # 移除舊版*.bat
-                    #os.system ('del *.bat')
                     wahiddl_old_bat_files = glob.glob ("*.bat")
                     for bat_file in wahiddl_old_bat_files:
                         if os.path.exists (bat_file):
@@ -237,7 +219,6 @@ if sys.platform == "win32":
                         else:
                             print ("不存在 wahid-dl 舊版之 .bat 檔案")
                     # 移除舊版*.py
-                    #os.system ('del *.py')
                     os.chdir ('C:\\wahid-dl')
                     wahiddl_old_py_files = glob.glob ("*.py")
                     for py_file in wahiddl_old_py_files:
@@ -250,7 +231,6 @@ if sys.platform == "win32":
                 # 安裝/更新新版wahid-dl主程式
                 os.chdir ('C:\\wahid-dl')
                 os.system ('curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main')
-                #os.mkdir ('updates')
                 updates_folder_name = 'updates'
                 if not os.path.exists (updates_folder_name):
                     os.mkdir (updates_folder_name)
@@ -259,8 +239,6 @@ if sys.platform == "win32":
                     print (f"{updates_folder_name} 更新資料之暫存資料夾已存在")
                 print ('解壓縮 wahid-dl 更新資料')
                 os.system ('tar -zxvf updates.zip -C "C:\\wahid-dl\\updates"')
-                #os.system ('move "C:\\wahid-dl\\updates\\wahid-dl-main\\*.bat" "C:\\wahid-dl\\"')
-                #os.system ('move "C:\\wahid-dl\\updates\\wahid-dl-main\\*.py" "C:\\wahid-dl\\"')
                 wahiddl_updatesfiles_folder = "C:\\wahid-dl\\updates\\wahid-dl-main\\"
                 wahiddl_dst_folder = "C:\\wahid-dl\\"
                 wahiddl_bat_files = glob.glob (wahiddl_updatesfiles_folder + "*.bat")
@@ -269,14 +247,12 @@ if sys.platform == "win32":
                 wahiddl_py_files = glob.glob (wahiddl_updatesfiles_folder + "*.py")
                 for wahiddl_py_file in wahiddl_py_files:
                     shutil.move (wahiddl_py_file, wahiddl_dst_folder)
-                #os.system ('del "C:\\wahid-dl\\updates.zip"')
                 wahiddl_updates_file_path = "C:\\wahid-dl\\updates.zip"
                 if os.path.isfile (wahiddl_updates_file_path) == True:
                     os.remove (wahiddl_updates_file_path)
                     print (f"更新資料 '{wahiddl_updates_file_path}' 已刪除")
                 else:
                     print (f"更新資料 '{wahiddl_updates_file_path}' 已不存在")
-                #os.system ('rd /s /q "C:\\wahid-dl\\updates"')
                 wahiddl_updates_folder_path = "C:\\wahid-dl\\updates"
                 if os.path.exists (wahiddl_updates_folder_path):
                     shutil.rmtree (wahiddl_updates_folder_path)
@@ -302,7 +278,6 @@ if sys.platform == "win32":
                     os.system ('yt-dlp -U')
                 print ("更新 yt-dlp 完成")
                 print ("------------------------------------------------------------")
-                #os.system ('pip install -U "yt-dlp[default]"')
                 subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
                 print ("更新 yt-dlp Pip Version 完成")
                 print ("------------------------------------------------------------")
@@ -316,7 +291,6 @@ if sys.platform == "win32":
                 os.chdir ('C:\\wahid-dl')
                 # 移除舊有ffmpeg安裝方式之檔案
                 # 移除舊式FFmpeg安裝之舊版
-                #os.system ('del ffmpeg.exe ffplay.exe ffprobe.exe')
                 if os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe') == True:
                     os.remove ('C:\\wahid-dl\\ffmpeg.exe')
                     print ("已刪除舊式舊版的 ffmpeg.exe")
@@ -336,14 +310,12 @@ if sys.platform == "win32":
                 os.chdir ('C:\\')
                 # 移除新式FFmpeg安裝之舊版
                 ffmpeg_old_ver_folder_path = "C:\\FFmpeg"
-                #os.mkdir ('FFmpeg')
                 ffmpeg_folder_name = 'FFmpeg'
                 if not os.path.exists (ffmpeg_folder_name):
                     os.mkdir (ffmpeg_folder_name)
                     print (f"新式 {ffmpeg_folder_name} 資料夾已建立")
                 else:
                     print (f"新式 {ffmpeg_folder_name} 資料夾已存在")
-                    #os.system ('del ffmpeg.exe ffplay.exe ffprobe.exe')
                     # 移除新式舊版ffmpeg.exe, ffplay.exe, ffprobe.exe
                     if os.path.isfile ('C:\\FFmpeg\\ffmpeg.exe') == True:
                         os.remove ('C:\\FFmpeg\\ffmpeg.exe')
@@ -363,7 +335,6 @@ if sys.platform == "win32":
                 # 安裝/更新新版FFmpeg主程式
                 os.chdir ('C:\\FFmpeg')
                 os.system ('curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/7.0.2/ffmpeg-7.0.2-full_build.zip')
-                #os.mkdir ('FFmpeg-unzip')
                 FFmpegunzip_folder_name = 'FFmpeg-unzip'
                 if not os.path.exists (FFmpegunzip_folder_name):
                     os.mkdir (FFmpegunzip_folder_name)
@@ -371,7 +342,6 @@ if sys.platform == "win32":
                 else:
                     print (f"'{FFmpegunzip_folder_name}' 資料夾已存在")
                 os.system ('tar -zxvf ffmpeg.zip -C "C:\\FFmpeg\\FFmpeg-unzip"')
-                #os.system ('move "C:\\FFmpeg\\FFmpeg-unzip\\ffmpeg-7.0.1-full_build\\bin\\*.exe" "C:\\FFmpeg\\"')
                 ffmpeg_updatesfiles_folder = "C:\\FFmpeg\\FFmpeg-unzip\\ffmpeg-7.0.1-full_build\\bin\\"
                 ffmpeg_dst_folder = "C:\\FFmpeg\\"
                 ffmpeg_exe_files = glob.glob (ffmpeg_updatesfiles_folder + "*.exe")
@@ -379,14 +349,12 @@ if sys.platform == "win32":
                     shutil.move (ffmpeg_exe_file, ffmpeg_dst_folder)
                 # FFmpeg系統環境變數設定
                 os.system ('setx PATH "FFmpeg;C:\\FFmpeg\\"')
-                #os.system ('del "C:\\FFmpeg\\ffmpeg.zip"')
                 ffmpeg_updates_file_path = "C:\\FFmpeg\\ffmpeg.zip"
                 if os.path.isfile (ffmpeg_updates_file_path) == True:
                     os.remove (ffmpeg_updates_file_path)
                     print (f"更新資料 '{ffmpeg_updates_file_path}' 已刪除")
                 else:
                     print (f"更新資料 '{ffmpeg_updates_file_path}' 已不存在")
-                #os.system ('rd /s /q "C:\\FFmpeg\\ffmpeg-unzip"')
                 ffmpeg_unzip_folder_path = "C:\\FFmpeg\\ffmpeg-unzip"
                 if os.path.exists (ffmpeg_unzip_folder_path):
                     shutil.rmtree (ffmpeg_unzip_folder_path)
@@ -431,7 +399,6 @@ if sys.platform == "win32":
                 print ("安裝/更新 wahid-dl DEV")
                 os.chdir ('C:\\wahid-dl DEV')
                 os.system ('curl -L -o updates.zip https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/dev')
-                #os.mkdir ('updates')
                 updates_folder_name = 'updates'
                 if not os.path.exists (updates_folder_name):
                     os.mkdir (updates_folder_name)
