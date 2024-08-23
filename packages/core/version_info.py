3152298f35337ce40660697b9b4ef1569309a1f1
chengmoxu <rolly200311@gmail.com>
2024-08-23 21:38:53 +0800

    feat: Added version_info module and related folders and changed version number naming rules

---
+++ packages/core/version_info.py
@@ -0,0 +1,9 @@
+version_outline = '[Dev] v4.7-20240823.1'
+version_number = '4.7'
+version_build_number = '20240823.1'
+version_channel = 'Dev'
+version_programming_language = 'Python'
+def get_version_outline():
+    return version_outline
+def get_version_detail():
+    return version_number, version_build_number, version_channel, version_programming_language
