d1f282fdb76ccfe9a768d12f04c23dfb510b56e0
chengmoxu <rolly200311@gmail.com>
2024-08-26 16:46:08 +0800

    style: Changed version number

--- packages/core/version_info.py
+++ packages/core/version_info.py
@@ -1,6 +1,6 @@
-version_outline = '[Dev] v4.7-20240824.1'
+version_outline = '[Dev] v4.7-20240826.1'
 version_number = '4.7'
-version_build_number = '20240824.1'
+version_build_number = '20240826.1'
 version_channel = 'Dev'
 version_programming_language = 'Python'
 def get_version_outline():
