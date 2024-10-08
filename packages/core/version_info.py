457e6bc9257a0a544cfdabaeb448299b3a855443
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-10-08 15:45:09 +0800

    style/feat: Changed version number/Added version information function

--- packages/core/version_info.py
+++ packages/core/version_info.py
@@ -1,9 +1,13 @@
-version_outline = "[Stable] v5.1-20240918.1"
-version_number = "5.1"
-version_build_number = "20240918.1"
-version_channel = "Stable"
+version_outline = "[Dev] v5.2-20241008.1"
+version_number = "5.2"
+version_build_number = "20241008.1"
+version_channel = "Dev"
 version_programming_language = "Python"
 def get_version_outline():
     return version_outline
 def get_version_detail():
     return version_number, version_build_number, version_channel, version_programming_language
+def get_version_channel():
+    return version_channel
+def get_version_programming_language():
+    return version_programming_language
