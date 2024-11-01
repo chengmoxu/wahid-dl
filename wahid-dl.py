a798d1662b81519765047e85995e864a1b9a595a
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-11-01 11:24:06 +0800

    feat: Added Linux support

--- wahid-dl.py
+++ wahid-dl.py
@@ -11,7 +11,6 @@
 # Library import
 import os
 import sys
-import subprocess
 from packages.core import title
 
 # Download Function within OS judge
@@ -45,40 +44,39 @@ if sys.platform == "win32":
             else:
                 print ("請重新輸入正確網址！")
     while mode == "0":
-        print("即將結束程式")
+        print("即將結束程式，稍後再次按下Enter鍵即可退出此程式")
         break
 elif sys.platform == "linux":
-    print ("wahid-dl Python-Based")
+    print (title.get_title_wahiddl())
     print ("------------------------------------------------------------")
     mode = ""
     while mode == "":
-        print ("請輸入欲下載影片之網址")
-        print ("或者，請輸入0結束程式\n")
-        userinput = input ("請輸入：")
+        print ("Please enter the URL of the video you want to download")
+        print ("Or enter '0' to end this program\n")
+        userinput = input ("Please enter:")
         userinput_judge = str.isdigit(userinput)
         if userinput_judge == True: 
             if  userinput == "0":
                 mode = "0"
             else:
-               print ("請重新輸入正確選項！")
+               print ("Please re-enter the correct options!")
         elif userinput_judge == False:
             if userinput.startswith("http") == True:
                 print ("------------------------------------------------------------")
-                print ("執行開始")
+                print ("Execution starts")
                 print ("------------------------------------------------------------")
-                subprocess.check_call ([sys.executable, '-m', 'pip', 'install', '-U', 'yt-dlp[default]'])
-                downloadcommand = str (('yt-dlp -c -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 --ffmpeg-location "C:\\FFmpeg" ') + userinput)
+                downloadcommand = str (('yt-dlp -c -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 ') + userinput)
                 os.system (downloadcommand)
                 print ("------------------------------------------------------------")
-                print ("執行結束，請至資料夾內確認您的下載")
+                print ("The execution is completed, please go to the folder to confirm your download")
                 print ("------------------------------------------------------------")
             elif userinput.startswith("http") == False:
-                print ("請重新輸入正確網址！")
+                print ("Please re-enter the correct URL!")
             else:
-                print ("請重新輸入正確網址！")
+                print ("Please re-enter the correct URL!")
     while mode == "0":
-        print("即將結束程式")
+        print("This program will come to a close, press the Enter key again later to exit this program")
         break
 #elif sys.platform == "darwin":
 #    print ("wahid-dl Python-Based")
-os.system ('pause')
+input()
