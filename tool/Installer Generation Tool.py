ecf725abf812b22ccca91eb9f7c981040201378f
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-11-17 00:25:32 +0800

    feat: Added folder creation function

--- tool/Installer Generation Tool.py
+++ tool/Installer Generation Tool.py
@@ -1,7 +1,7 @@
-# Installer Generation Tool
-# [Dev] v5.3-20241116.1
+# wahid-dl Installer Generation Tool
 import os
 import sys
+import time
 from pathlib import Path
 def checking_wahiddl_packages_folder_existed():
     if sys.platform  == "win32":
@@ -21,3 +21,12 @@ def checking_wahiddl_tool_existed():
         return wahiddl_tool_path.is_file(), None     
     except Exception as error:
         return False, f"Error checking wahid-dl Tool.py: {str(error)}"
+def create_folder():
+    version_outline = "Dev_v5.3_20241117.1"
+    exact_time = time.ctime(time.time())
+    folder_name = "wahid-dl Installer Generation Tool" + "_" + version_outline + "(" + exact_time + ")"
+    if not os.path.exists (folder_name):
+        os.mkdir (folder_name)
+    else:
+        os.rmdir (folder_name)
+        os.mkdir (folder_name)
