d0f5a25701ee9b89b8d6e7514cba7140649638c7
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-11-18 14:19:05 +0800

    fix: Fixed wrong file path

--- packages/checker/checking_ytdlp.py
+++ packages/checker/checking_ytdlp.py
@@ -2,13 +2,13 @@ import subprocess
 from pathlib import Path
 def checking_wahiddl_ytdlp_existed():
     try:
-        wahiddl_ytdlp_path = Path('C:/wahid-dl/yt-dlp.exe')
+        wahiddl_ytdlp_path = Path("C:\\wahid-dl\\yt-dlp.exe")
         return wahiddl_ytdlp_path.is_file()
     except Exception as error:
         return False
 def checking_wahiddl_DEV_ytdlp_existed():
     try:
-        wahiddl_DEV_ytdlp_path = Path('C:/wahid-dl DEV/yt-dlp.exe')
+        wahiddl_DEV_ytdlp_path = Path("C:\\wahid-dl DEV\\yt-dlp.exe")
         return wahiddl_DEV_ytdlp_path.is_file()
     except Exception as error:
         return False
