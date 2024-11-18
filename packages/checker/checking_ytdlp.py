8dbde621f6f3f59be71432bcd065f1c6a66cd674
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-11-18 18:10:09 +0800

    fix: Removed redundant returns

--- packages/checker/checking_ytdlp.py
+++ packages/checker/checking_ytdlp.py
@@ -4,13 +4,13 @@ def checking_wahiddl_ytdlp_existed():
     try:
         wahiddl_ytdlp_path = Path("C:\\wahid-dl\\yt-dlp.exe")
         return wahiddl_ytdlp_path.is_file()
-    except Exception as error:
+    except Exception:
         return False
 def checking_wahiddl_DEV_ytdlp_existed():
     try:
         wahiddl_DEV_ytdlp_path = Path("C:\\wahid-dl DEV\\yt-dlp.exe")
         return wahiddl_DEV_ytdlp_path.is_file()
-    except Exception as error:
+    except Exception:
         return False
 def checking_linux_ytdlp_existed():
     try:
