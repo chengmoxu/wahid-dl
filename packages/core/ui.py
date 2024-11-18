797e48230efea8c7cd52e62e480ac1417760fd7d
ChengMo <99020504+chengmoxu@users.noreply.github.com>
2024-11-18 22:23:16 +0800

    feat: Added function to display version number

--- packages/core/ui.py
+++ packages/core/ui.py
@@ -10,14 +10,14 @@ def ASCII_art():
            "####      ##     ###      ##     ###     ###     #######          #########            #########       ###")
 system_os = system_os.get_system_os()
 programming_language = version_info.get_version_programming_language()
-title_wahiddl = "wahid-dl [" + system_os + "] [" + programming_language + "]"
-title_wahiddl_tool = "wahid-dl Tool [" + system_os + "] [" + programming_language + "]"
-title_wahiddl_devmode = "wahid-dl Dev Mode [" + system_os + "] [" + programming_language + "]" + "\n" + version_info.get_version_outline()
 def get_title_wahiddl():
+    title_wahiddl = "wahid-dl [" + system_os + "] [" + programming_language + "]"+ "\n" + version_info.get_version_outline()
     return title_wahiddl
 def get_title_wahiddl_tool():
+    title_wahiddl_tool = "wahid-dl Tool [" + system_os + "] [" + programming_language + "]"+ "\n" + version_info.get_version_outline()
     return title_wahiddl_tool
 def get_title_wahiddl_devmode():
+    title_wahiddl_devmode = "wahid-dl Dev Mode [" + system_os + "] [" + programming_language + "]" + "\n" + version_info.get_version_outline()
     return title_wahiddl_devmode
 def ui_start():
     print ("------------------------------------------------------------")
