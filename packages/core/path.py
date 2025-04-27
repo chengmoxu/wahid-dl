afe0fc93ec41f1e75ba2599eaa1fb2f43d77ad28
chengmoxu <rolly200311@gmail.com>
2025-04-28 05:06:23 +0800

    feat: Added unified path

---
+++ packages/core/path.py
@@ -0,0 +1,12 @@
+def wahiddl_folder():
+    path = 'C:\\wahid-dl'
+    return path
+def wahiddl_DEV_folder():
+    path = 'C:\\wahid-dl DEV'
+    return path
+def ffmpeg_folder():
+    path = 'C:\\FFmpeg'
+    return path
+def old_ffmpeg_files():
+    path = ['C:\\wahid-dl\\ffmpeg.exe', 'C:\\wahid-dl\\ffplay.exe', 'C:\\wahid-dl\\ffprobe.exe']
+    return path
