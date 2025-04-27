fd85a593110a7e1d4addd7258982fa7cfeb5c074
chengmoxu <rolly200311@gmail.com>
2025-04-28 04:32:25 +0800

    feat: Changed to use unified packages

--- wahid-dl for Audio.py
+++ wahid-dl for Audio.py
@@ -1,73 +1,12 @@
-import os
-import sys
-from packages.core import ui
-
-if sys.platform == "win32":
-    print (ui.ASCII_art())
-    print (ui.ui_title_wahiddl_audio())
-    print ("------------------------------------------------------------")
-    mode = ""
-    while mode != "0":
-        print ("請輸入欲下載音訊之網址")
-        print ("或者，請輸入0結束程式\n")
-        userinput = input ("請輸入：")
-        userinput_judge = str.isdigit(userinput)
-        if userinput_judge == True: 
-            if  userinput == "0":
-                mode = "0"
-            else:
-               print ("請重新輸入正確選項！")
-        elif userinput_judge == False:
-            if userinput.startswith("http") == True:
-                print ("------------------------------------------------------------")
-                print ("執行開始")
-                print ("------------------------------------------------------------")
-                os.chdir ('C:\\wahid-dl')
-                downloadcommand = str (('yt-dlp -c --throttled-rate 100K --extract-audio -f "bestaudio[ext=m4a]" ') + userinput)
-                os.system (downloadcommand)
-                print ("------------------------------------------------------------")
-                print ("執行結束，請至資料夾內確認您的下載")
-                print ("------------------------------------------------------------")
-            elif userinput.startswith("http") == False:
-                print ("請重新輸入正確網址！")
-            else:
-                print ("請重新輸入正確網址！")
-    while mode == "0":
-        print("即將結束程式")
-        break
-elif sys.platform == "linux":
-    print (ui.ASCII_art())
-    print (ui.ui_title_wahiddl_audio())
-    print ("------------------------------------------------------------")
-    mode = ""
-    while mode != "0":
-        print ("請輸入欲下載音訊之網址")
-        print ("或者，請輸入0結束程式\n")
-        userinput = input ("請輸入：")
-        userinput_judge = str.isdigit(userinput)
-        if userinput_judge == True: 
-            if  userinput == "0":
-                mode = "0"
-            else:
-               print ("請重新輸入正確選項！")
-        elif userinput_judge == False:
-            if userinput.startswith("http") == True:
-                print ("------------------------------------------------------------")
-                print ("執行開始")
-                print ("------------------------------------------------------------")
-                os.chdir ('C:\\wahid-dl')
-                
-                downloadcommand = str (('yt-dlp -c --throttled-rate 100K --extract-audio -f "bestaudio[ext=m4a]" ') + userinput)
-                os.system (downloadcommand)
-                print ("------------------------------------------------------------")
-                print ("執行結束，請至資料夾內確認您的下載")
-                print ("------------------------------------------------------------")
-            elif userinput.startswith("http") == False:
-                print ("請重新輸入正確網址！")
-            else:
-                print ("請重新輸入正確網址！")
-    while mode == "0":
-        print("即將結束程式")
-        break
-#elif sys.platform == "darwin":
-os.system ('pause')
+from packages.core import version_info
+from packages.function import wahiddl_audio
+system_os = version_info.vi_platform()
+if system_os  == "Windows":
+    wahiddl_audio.main()
+elif system_os  == "Linux":
+    print ("暫時不支援")
+elif system_os == "macOS":
+    print ("暫時不支援")
+else:
+    print ("不支援的平台")
+input()
