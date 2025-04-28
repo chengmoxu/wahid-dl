1707224dee7cb90aff70e6a679958a34a8ad0bea
chengmoxu <rolly200311@gmail.com>
2025-04-28 20:18:53 +0800

    fix: Fixed incorrect text

--- wahid-dl Tool.py
+++ wahid-dl Tool.py
@@ -6,7 +6,7 @@ import glob
 import re
 import importlib
 
-version_outline = "[Develop]v6.0-20250428.1-Python"
+version_outline = "[Develop]v6.0.20250428.1-Python"
 
 def wahiddl_installer_without_packages():
     if os.path.exists ("C:\\wahid-dl"):
