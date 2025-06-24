48fb51e0e13b91a8eaffd1ef643e4d0cd5c85f5c
chengmoxu <rolly200311@gmail.com>
2025-06-24 17:33:24 +0800

    fix: Fixed uninstall functionality

--- packages/checker/checking_ffmpeg.py
+++ packages/checker/checking_ffmpeg.py
@@ -6,74 +6,39 @@ def checking_ffmpeg_folder_existed():
     os.chdir ('C:\\')
     if os.path.exists (ffmpeg_folder_name):
         ffmpeg_folder_existed = True
-        error_location = False
     elif not os.path.exists (ffmpeg_folder_name):
         ffmpeg_folder_existed = False
-        error_location = False
-    else:
-        ffmpeg_folder_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_folder_existed()'
-    return ffmpeg_folder_existed, error_location
+    return ffmpeg_folder_existed
 def checking_ffmpeg_files_existed():
     os.chdir ('C:\\FFmpeg\\')
     if os.path.isfile ('C:\\FFmpeg\\ffmpeg.exe'):
         ffmpeg_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\FFmpeg\\ffmpeg.exe'):
         ffmpeg_exe_existed = False
-        error_location = False
-    else:
-        ffmpeg_exe_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_files_existed()'
     if os.path.isfile ('C:\\FFmpeg\\ffplay.exe'):
         ffplay_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\FFmpeg\\ffplay.exe'):
         ffplay_exe_existed = False
-        error_location = False
-    else:
-        ffplay_exe_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_files_existed()'
     if os.path.isfile ('C:\\FFmpeg\\ffprobe.exe'):
         ffprobe_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\FFmpeg\\ffprobe.exe'):
         ffprobe_exe_existed = False
-        error_location = False
-    else:
-        ffprobe_exe_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_files_existed()'
-    return ffmpeg_exe_existed, ffplay_exe_existed, ffprobe_exe_existed, error_location
+    return ffmpeg_exe_existed, ffplay_exe_existed, ffprobe_exe_existed
 def checking_old_ffmpeg_files_existed():
     os.chdir ('C:\\wahid-dl\\')
     if os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe'):
         ffmpeg_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe'):
         ffmpeg_exe_existed = False
-        error_location = False
-    else:
-        ffmpeg_exe_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_files_existed()'
     if os.path.isfile ('C:\\wahid-dl\\ffplay.exe'):
         ffplay_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\wahid-dl\\ffplay.exe'):
         ffplay_exe_existed = False
-        error_location = False
-    else:
-        ffplay_exe_existed = 'Internal Error'
-        error_location = 'checking_ffmpeg_files_existed()'
     if os.path.isfile ('C:\\wahid-dl\\ffprobe.exe'):
         ffprobe_exe_existed = True
-        error_location = False
     elif not os.path.isfile ('C:\\wahid-dl\\ffprobe.exe'):
         ffprobe_exe_existed = False
-        error_location = False
-    else:
-        ffprobe_exe_existed = 'Internal Error'
-        error_location = 'checking_old_ffmpeg_files_existed()'
-    return ffmpeg_exe_existed, ffplay_exe_existed, ffprobe_exe_existed, error_location
+    return ffmpeg_exe_existed, ffplay_exe_existed, ffprobe_exe_existed
 def checking_ffmpeg_version():
     ffmpeg_exact_version_number = '7.1.1'
     ffmpeg_version_org_output = subprocess.getoutput ('ffmpeg -version')
--- packages/uninstaller/uninstaller_ffmpeg.py
+++ packages/uninstaller/uninstaller_ffmpeg.py
@@ -6,28 +6,16 @@ def uninstaller_ffmpeg():
         print ("已刪除新式舊版的 ffmpeg.exe")
     elif not os.path.isfile ('C:\\FFmpeg\\ffmpeg.exe') == True:
         print ("已不存在新式舊版 ffmpeg.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_ffmpeg()'
-        return error_location
     if os.path.isfile ('C:\\FFmpeg\\ffplay.exe') == True:
         os.remove ('C:\\FFmpeg\\ffplay.exe')
         print ("已刪除新式舊版的 ffplay.exe")
     elif not os.path.isfile ('C:\\FFmpeg\\ffplay.exe') == True:
         print ("已不存在新式舊版 ffplay.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_ffmpeg()'
-        return error_location
     if os.path.isfile ('C:\\FFmpeg\\ffprobe.exe') == True:
         os.remove ('C:\\FFmpeg\\ffprobe.exe')
         print ("已刪除新式舊版的 ffprobe.exe")
     elif not os.path.isfile ('C:\\FFmpeg\\ffprobe.exe') == True:
         print ("已不存在新式舊版 ffprobe.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_ffmpeg()'
-        return error_location
 def uninstaller_old_ffmpeg():
     os.chdir ('C:\\wahid-dl')
     if os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe') == True:
@@ -35,25 +23,13 @@ def uninstaller_old_ffmpeg():
         print ("已刪除舊式舊版的 ffmpeg.exe")
     elif not os.path.isfile ('C:\\wahid-dl\\ffmpeg.exe') == True:
         print ("已不存在舊式舊版 ffmpeg.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_old_ffmpeg()'
-        return error_location
     if os.path.isfile ('C:\\wahid-dl\\ffplay.exe') == True:
         os.remove ('C:\\wahid-dl\\ffplay.exe')
         print ("已刪除舊式舊版的 ffplay.exe")
     elif not os.path.isfile ('C:\\wahid-dl\\ffplay.exe') == True:
         print ("已不存在舊式舊版 ffplay.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_old_ffmpeg()'
-        return error_location
     if os.path.isfile ('C:\\wahid-dl\\ffprobe.exe') == True:
         os.remove ('C:\\wahid-dl\\ffprobe.exe')
         print ("已刪除舊式舊版的 ffprobe.exe")
     elif not os.path.isfile ('C:\\wahid-dl\\ffprobe.exe') == True:
         print ("已不存在舊式舊版 ffprobe.exe")
-    else:    
-        print ('Internal Error')
-        error_location = 'uninstaller_old_ffmpeg()'
-        return error_location
