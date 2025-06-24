9d4e466851119c43ac9272217932273a7334cb3e
chengmoxu <rolly200311@gmail.com>
2025-06-24 16:59:06 +0800

    fix: Modified the judgment value to Boolean value

--- packages/checker/checking_internet.py
+++ packages/checker/checking_internet.py
@@ -4,9 +4,7 @@ def get_internet_status():
     github_url = 'https://github.com/'
     github_response = requests.get (github_url, timeout=5)
     if github_response.status_code != 200:
-        print("目前無法連上網際網路")
-        internet_status = "False"  
+        internet_status = False  
     elif github_response.status_code == 200:
-        print("目前已連上網際網路")
-        internet_status = "True"
+        internet_status = True
     return internet_status
