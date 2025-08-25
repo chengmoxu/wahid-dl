55f790d15aea4d4ccce876237fb9f9bbfb16655b
chengmoxu <rolly200311@gmail.com>
2025-08-26 00:31:11 +0800

    style: Temporarily remove unused code and added comment

--- wahid-dl.py
+++ wahid-dl.py
@@ -1,8 +1,8 @@
-from packages.core import version_info
+#from packages.core import version_info
 from packages.function import wahiddl
-system_os = version_info.vi_platform()
-try: 
-    wahiddl.main()
+#system_os = version_info.vi_platform() # from import 'version_info' checking current platform and
+try:
+    wahiddl.main() # from packages.function import the 'wahiddl'and execute its function main()
 except:
     print ("不支援的平台")
 input()
