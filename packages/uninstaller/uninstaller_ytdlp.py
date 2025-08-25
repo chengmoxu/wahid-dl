3bf67545056467cdbe1fd4149df6c73e9862808a
chengmoxu <rolly200311@gmail.com>
2025-08-26 00:28:50 +0800

    fix: Fixed incorrect code

--- packages/uninstaller/uninstaller_ytdlp.py
+++ packages/uninstaller/uninstaller_ytdlp.py
@@ -5,7 +5,6 @@ def uninstaller_wahiddl_ytdlp_uninstall():
         os.remove ('C:\\wahid-dl\\yt-dlp.exe')
         print ("已刪除 yt-dlp.exe")
     elif not os.path.isfile ('C:\\wahid-dl\\yt-dlp.exe') == True:
-        os.remove ('C:\\wahid-dl\\yt-dlp.exe')
         print ("不存在 yt-dlp.exe")
 def uninstaller_wahiddl_DEV_ytdlp_uninstall():
     os.chdir ('C:\\wahid-dl DEV')
@@ -13,5 +12,4 @@ def uninstaller_wahiddl_DEV_ytdlp_uninstall():
         os.remove ('C:\\wahid-dl DEV\\yt-dlp.exe')
         print ("已刪除 yt-dlp.exe")
     elif not os.path.isfile ('C:\\wahid-dl DEV\\yt-dlp.exe') == True:
-        os.remove ('C:\\wahid-dl DEV\\yt-dlp.exe')
         print ("不存在 yt-dlp.exe")
