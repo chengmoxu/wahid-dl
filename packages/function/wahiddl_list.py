57e97bb222e3b77df62e37f7c5278be3a026aff8
chengmoxu <rolly200311@gmail.com>
2025-08-26 21:54:02 +0800

    feat: Called the unified path function

--- packages/function/wahiddl_list.py
+++ packages/function/wahiddl_list.py
@@ -1,5 +1,6 @@
 import os
 from packages.core import ui
+from packages.core import path #unified path function
 def main():
     print(ui.ASCII_art())
     print(ui.ui_title_wahiddl_list())
@@ -16,14 +17,14 @@ def main():
             elif userinput == "1":
                 x = 0
                 for x in range (0,2,1):
-                    if os.path.isfile ('C:\\wahid-dl\\url_list.txt') == True:
+                    if os.path.isfile (path.wahiddl_url_list()) == True:
                         print ("存在url_list.txt")
                         print ('刪除存在的url_list.txt，並自動建立新的url_list.txt，請輸入1')
                         print ('保留存在的url_list.txt，並以此清單進行下載，請輸入2')
                         choice = input ('請選擇處理方式：')
                         if choice == 1:
-                            os.remove ('C:\\wahid-dl\\url_list.txt')
-                            os.open('C:\\wahid-dl\\url_list.txt', os.O_RDWR|os.O_CREAT)
+                            os.remove (path.wahiddl_url_list())
+                            os.open(path.wahiddl_url_list(), os.O_RDWR|os.O_CREAT)
                             print ('請在對於url_list.txt編輯完成後再按下任一鍵以進行下載')
                             os.system ('pause')
                             break
@@ -36,12 +37,12 @@ def main():
                     else:
                         print ("不存在url_list.txt")
                         print ('已自動建立新的url_list.txt')
-                        os.open('C:\\wahid-dl\\url_list.txt', os.O_RDWR|os.O_CREAT)
+                        os.open(path.wahiddl_url_list(), os.O_RDWR|os.O_CREAT)
                         print ('請在對於url_list.txt編輯完成後再按下任一鍵以進行下載')
                         os.system ('pause')
                         break
                 ui.ui_start()
-                os.chdir ('C:\\wahid-dl')
+                os.chdir (path.wahiddl_folder())
                 downloadcommand = str (('yt-dlp --batch-file url_list.txt -c -S"quality,res,fps,hdr:12,channels,size,br,asr" --throttled-rate 100K --merge-output-format mp4 --ffmpeg-location "C:\\FFmpeg"'))
                 os.system (downloadcommand)
                 ui.ui_complete()
