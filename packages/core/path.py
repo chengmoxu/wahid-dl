5d2ed7e6bfa6edd5ebf0f2802a6b629c33a92d30
chengmoxu <rolly200311@gmail.com>
2025-08-26 12:49:18 +0800

    feat: Added new paths

--- packages/core/path.py
+++ packages/core/path.py
@@ -1,14 +1,20 @@
+def root():
+    path = 'C:\\'
+    return path
 def wahiddl_folder():
     path = 'C:\\wahid-dl'
     return path
 def wahiddl_DEV_folder():
     path = 'C:\\wahid-dl DEV'
     return path
+def old_ffmpeg_files():
+    path = ['C:\\wahid-dl\\ffmpeg.exe', 'C:\\wahid-dl\\ffplay.exe', 'C:\\wahid-dl\\ffprobe.exe']
+    return path
 def ffmpeg_folder():
     path = 'C:\\FFmpeg'
     return path
-def old_ffmpeg_files():
-    path = ['C:\\wahid-dl\\ffmpeg.exe', 'C:\\wahid-dl\\ffplay.exe', 'C:\\wahid-dl\\ffprobe.exe']
+def ffmpeg_files():
+    path = ['C:\\FFmpeg\\ffmpeg.exe', 'C:\\FFmpeg\\ffplay.exe', 'C:\\FFmpeg\\ffprobe.exe']
     return path
 def wahiddl_ytdlp():
     path = 'C:\\wahid-dl\\yt-dlp.exe'
