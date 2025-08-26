d60e92526d1d523aa49f9c34c0728ae233c1a15f
chengmoxu <rolly200311@gmail.com>
2025-08-26 09:00:32 +0800

    perf: Optimized function

--- packages/uninstaller/uninstaller_wahiddl_DEV.py
+++ packages/uninstaller/uninstaller_wahiddl_DEV.py
@@ -6,35 +6,35 @@ def uninstaller_wahiddl_DEV_uninstall():
     os.chdir (path.wahiddl_DEV_folder())
     wahiddl_DEV_old_bat_files = glob.glob ("*.bat")
     for bat_file in wahiddl_DEV_old_bat_files:
-        if os.path.exists (bat_file):
+        try:
             os.remove (bat_file)
             print (f"已刪除 wahid-dl Develop Channel 舊版的{bat_file}")
-        else:
+        except:
             print ("不存在 wahid-dl Develop Channel 舊版之 .bat 檔案")
     wahiddl_DEV_old_py_files = glob.glob ("*.py")
     for py_file in wahiddl_DEV_old_py_files:
-        if os.path.exists (py_file):
+        try:
             os.remove (py_file)
             print (f"已刪除 wahid-dl Develop Channel 舊版的{py_file}")
-        else:
+        except:
             print ("不存在 wahid-dl Develop Channel 舊版之 .py 檔案")
-    if os.path.exists (path.wahiddl_DEV_packages()):
+    try:
         shutil.rmtree(path.wahiddl_DEV_packages())
-        print (f"已刪除 wahid-dl Develop Channel 舊版的 packages")
-    elif not os.path.exists (path.wahiddl_DEV_packages()):
-        print (f"已不存在 wahid-dl Develop Channel 舊版的 packages")
-    if os.path.exists (path.wahiddl_DEV_bat()):
+        print ("已刪除 wahid-dl Develop Channel 舊版的 packages")
+    except:
+        print ("已不存在 wahid-dl Develop Channel 舊版的 packages")
+    try:
         shutil.rmtree(path.wahiddl_DEV_bat())
-        print (f"已刪除 wahid-dl Develop Channel 的早期版本支援")
-    elif not os.path.exists (path.wahiddl_DEV_bat()):
-        print (f"已不存在 wahid-dl Develop Channel 的早期版本支援")
-    if os.path.exists (path.wahiddl_DEV_tool()):
+        print ("已刪除 wahid-dl Develop Channel 的早期版本支援")
+    except:
+        print ("已不存在 wahid-dl Develop Channel 的早期版本支援")
+    try:
         shutil.rmtree(path.wahiddl_DEV_tool())
-        print (f"已刪除 wahid-dl Develop Channel 的安裝檔打包工具資料夾")
-    elif not os.path.exists (path.wahiddl_DEV_tool()):
-        print (f"已不存在 wahid-dl Develop Channel 的安裝檔打包工具資料夾")
-    if os.path.isfile (path.wahiddl_DEV_readme()) == True:
+        print ("已刪除 wahid-dl Develop Channel 的安裝檔打包工具資料夾")
+    except:
+        print ("已不存在 wahid-dl Develop Channel 的安裝檔打包工具資料夾")
+    try:
         os.remove (path.wahiddl_DEV_readme())
-        print (f"已刪除 README.md")
-    elif not os.path.isfile (path.wahiddl_DEV_readme()) == True:
-        print (f"已不存在 README.md")
+        print ("已刪除 README.md")
+    except:
+        print ("已不存在 README.md")
