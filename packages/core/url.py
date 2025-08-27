1574407b94ca546b3e00bdbc39bbdec5765b98dc
chengmoxu <rolly200311@gmail.com>
2025-08-27 11:27:05 +0800

    feat: Added the unified url function

---
+++ packages/core/url.py
@@ -0,0 +1,9 @@
+def wahiddl_main_branch():
+    url = 'https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/main'
+    return url
+def wahiddl_DEV_branch():
+    url = 'https://codeload.github.com/chengmoxu/wahid-dl/zip/refs/heads/develop'
+    return url
+def ffmpeg():
+    url = 'https://github.com/GyanD/codexffmpeg/releases/download/7.1.1/ffmpeg-7.1.1-full_build.zip'
+    return url
