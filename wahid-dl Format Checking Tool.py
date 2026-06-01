1a34df703e7ae3c679fa39909837a15d3bcca46e
chengmoxu <rolly200311@gmail.com>
2026-06-01 22:46:24 +0800

    fix: fix exit flow and optimize error prompts

--- wahid-dl Format Checking Tool.py
+++ wahid-dl Format Checking Tool.py
@@ -2,5 +2,4 @@ try:
     from packages.function import wahiddl_FCT
     wahiddl_FCT.main()
 except:
-    print("不支援的平台或wahid-dl安裝不完整")
-input()
+    print("執行終止：經使用者終止，或為不支援的平台、wahid-dl安裝不完整或程式發生問題")
