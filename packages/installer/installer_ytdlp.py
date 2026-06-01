b8f60c4731470507c81b8977478ee7b124bcf740
chengmoxu <rolly200311@gmail.com>
2026-06-01 23:28:18 +0800

    refactor: replace deprecated os.system with subprocess.run

--- packages/installer/installer_ytdlp.py
+++ packages/installer/installer_ytdlp.py
@@ -1,13 +1,14 @@
 import os
+import subprocess
 def install():
     os.chdir('C:\\wahid-dl\\')
-    os.system('curl -L -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe')
+    subprocess.run(['curl', '-L', '-o', 'yt-dlp.exe', 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe'])
 def update():
     os.chdir('C:\\wahid-dl\\')
-    os.system('yt-dlp -U')
+    subprocess.run(['yt-dlp', '-U'])
 def beta_install():
     os.chdir('C:\\wahid-dl (Beta)\\')
-    os.system('curl -L -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe')
+    subprocess.run(['curl', '-L', '-o', 'yt-dlp.exe', 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe'])
 def beta_update():
     os.chdir('C:\\wahid-dl (Beta)\\')
-    os.system('yt-dlp -U')
+    subprocess.run(['yt-dlp', '-U'])
